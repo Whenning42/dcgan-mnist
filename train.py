bb4fd6c25462704e976c31fcd26b9366979ea01c
William Henning <Henningw@umich.edu>
2019-04-07 15:04:29 -0400

    More changes

--- train.py
+++ train.py
@@ -9,6 +9,9 @@ from keras.optimizers import SGD, Adam
 from keras.utils import plot_model
 from visualizer import *
 from keras import backend as K
+from tensorflow.python.client import timeline
+import matplotlib.pyplot as plt
+
 import keras
 import skyrogue_loader
 
@@ -22,9 +25,9 @@ set_session(sess)  # set this TensorFlow session as the default session for Kera
 
 # After we have a seemingly working impl we can play around with fp16
 #K.set_floatx('float16')
-#K.set_epsilon(1e-3)
+#K.set_epsilon(1e-4)
 
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 #BATCH_SIZE = 8
 #NUM_EPOCH = 50
 NUM_EPOCH = 50000
@@ -33,7 +36,7 @@ B1 = 0.5  # momentum term
 GENERATED_IMAGE_PATH = 'images/'
 GENERATED_MODEL_PATH = 'models/'
 
-
+profile = False
 
 from PIL import Image
 
@@ -46,7 +49,7 @@ def train():
         #(X_train, y_train), (_, _) = mnist.load_data()
         #(X_train, _), (_, _) = skyrogue.load_data(160, 120)
         X_train = skyrogue_loader.load_images()
-        X_train = X_train[100:5100, :, :]
+#        X_train = X_train[100:5100, :, :]
         # normalize images
         print("Started normalizing images")
         X_train = (X_train.astype(np.float16) - 127.5)/127.5
@@ -63,24 +66,38 @@ def train():
         Image.fromarray(((X_train[i, :, :, 0] + 1) * 127.5).astype('uint8')).show()
         _ = input()
 
+    if profile:
+        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+        run_metadata= tf.RunMetadata()
+    else:
+        run_options = None
+        run_metadata = None 
+
+
     # build GAN
     g = generator()
+    # g = keras.models.load_model(GENERATED_MODEL_PATH+'dcgan_generator.h5')
     plot_model(g, to_file='generator.png')
 
     d = discriminator()
+    # d = keras.models.load_model(GENERATED_MODEL_PATH+'dcgan_discriminator.h5')
     plot_model(d, to_file='discriminator.png')
 
     opt = Adam(lr=LR,beta_1=B1)
     d.compile(loss='binary_crossentropy',
               metrics=['accuracy'],
-              optimizer=opt)
+              optimizer=opt,
+              options=run_options,
+              run_metadata=run_metadata)
 
     d.trainable = False
     dcgan = Sequential([g, d])
     opt= Adam(lr=LR,beta_1=B1)
     dcgan.compile(loss='binary_crossentropy',
                   metrics=['accuracy'],
-                  optimizer=opt)
+                  optimizer=opt,
+                  options=run_options,
+                  run_metadata=run_metadata)
 
     num_batches = int(X_train.shape[0] / BATCH_SIZE)
     # create directory
@@ -94,36 +111,69 @@ def train():
     print("-------------------")
     #z_pred = np.array([np.random.uniform(-1,1,100) for _ in range(49)])
     z_pred = np.array([np.random.normal(0, 0.5, 100) for _ in range(49)])
-    y_g = [1]*BATCH_SIZE
-    y_d_true = [1]*BATCH_SIZE
-    y_d_gen = [0]*BATCH_SIZE
+
+    d_epoch_losses = []
+    g_epoch_losses = []
     for epoch in list(map(lambda x: x+1, range(NUM_EPOCH))):
-        fuzz = max((200 - epoch) / 200, 0)
-    #    fuzz = 0
+        # Add noise to the images
+        image_fuzz = .5 ** (epoch / 30)
+        #fuzz = 0
+
+        # Use noisey labels
+        label_fuzz = image_fuzz
         
+        d_batch_losses = []
+        g_batch_losses = []
+
         shuffled_indices = np.random.permutation(X_train.shape[0])
         for index in range(num_batches):
+            y_g = np.ones(BATCH_SIZE)
+            y_d_true = np.ones(BATCH_SIZE)
+            y_d_gen = np.zeros(BATCH_SIZE)
+            for arr in [y_g, y_d_true, y_d_gen]:
+                random_label = np.random.choice([0, 1], BATCH_SIZE, p=[.5, .5])
+                label_mask = np.random.choice([0, 1], BATCH_SIZE, p=[1 - label_fuzz, label_fuzz]) == 1
+                arr[label_mask] = random_label[label_mask]
+
             X_d_true = X_train[shuffled_indices[index*BATCH_SIZE:(index+1)*BATCH_SIZE]]
-            X_d_fuzz = X_d_true * (1-fuzz) + fuzz * np.random.uniform(-1, 1, [BATCH_SIZE, 240, 320, 1])
+            X_d_fuzz = X_d_true * (1 - image_fuzz) + label_fuzz * np.random.uniform(-1, 1, [BATCH_SIZE, 240, 320, 1])
 
             X_g = np.array([np.random.normal(0, 0.5, 100) for _ in range(BATCH_SIZE)])
             X_d_gen = g.predict(X_g, verbose=0)
 
-            #h = d.predict(X_d_fuzz)
-            dr_loss = d.train_on_batch(X_d_fuzz, y_d_true)
-            #print()
-            #print(h)
-            #print(d.metrics_names)
-            #print(K.eval(keras.losses.binary_crossentropy(K.variable(np.array(y_d_true)), K.variable(np.array(h[:, 0])))))
+            d_real_loss, d_real_acc = d.train_on_batch(X_d_fuzz, y_d_true)
+            d_generated_loss, d_generated_acc = d.train_on_batch(X_d_gen, y_d_gen)
+            d_batch_losses.append((d_generated_loss + d_real_loss) / 2)
 
-            df_loss = d.train_on_batch(X_d_gen, y_d_gen)
-            g_loss = dcgan.train_on_batch(X_g, y_g)
+            g_loss, g_acc = dcgan.train_on_batch(X_g, y_g)
+            g_batch_losses.append(g_loss)
 
-            show_progress(epoch, index, g_loss[0], dr_loss[0], df_loss[0], g_loss[1], dr_loss[1], df_loss[1])
+            show_progress(epoch, index, g_loss, d_real_loss, d_generated_loss,
+                          g_acc, d_real_acc, d_generated_acc)
 
-        # save generated images
+        plt.clf()
+        d_epoch_losses.append(np.mean(d_batch_losses))
+        g_epoch_losses.append(np.mean(g_batch_losses))
+        plt.plot(d_epoch_losses, label="Discriminator loss")
+        plt.plot(g_epoch_losses, label="Generator loss")
+        plt.legend()
+        
+        plt.yscale("linear")
+        plt.savefig('loss_chart.png')
+
+        plt.yscale("log")
+        plt.savefig('log_loss_chart.png')
+
+        if profile:
+            tl = timeline.Timeline(run_metadata.step_stats)
+            ctf = tl.generate_chrome_trace_format()
+            with open('timeline.json', 'w') as f:
+                f.write(ctf)
+
+        # Checkpoint the model and write save an image of the model's generated output
         if epoch % 10 == 0:
-            print("Fuzz factor:", fuzz)
+            print()
+            print("Fuzz factor:", image_fuzz)
             image = combine_images(g.predict(z_pred))
             image = image*127.5 + 127.5
             Image.fromarray(image.astype(np.uint8))\
@@ -131,7 +181,6 @@ def train():
             print()
             # save models
             g.save(GENERATED_MODEL_PATH+'dcgan_generator.h5')
-            d.save(GENERATED_MODEL_PATH+'dcgan_discriminator.h5')
-
+            d.save(GENERATED_MODEL_PATH+'dcgan_discriminator.h5') 
 if __name__ == '__main__':
     train()
